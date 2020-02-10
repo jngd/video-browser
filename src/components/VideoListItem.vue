377ddb2f290cacdaf426b362655ebf830288ee76
jngd <juangrdiaz@gmail.com>
2020-02-10 06:20:22 +0100

    Thumbnails added

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,5 +1,8 @@
 <template>
-  <li class="list-group-item">{{ video.snippet.title }}</li>
+  <li class="list-group-item media">
+    <img class="mr-3" :src="thumbnailUrl" />
+    <div class="media-body">{{ video.snippet.title }}</div>
+  </li>
 </template>
 
 <script>
@@ -8,5 +11,21 @@ export default {
   props: {
     video: Object,
   },
+  computed: {
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    },
+  },
 };
 </script>
+
+<style scoped>
+li {
+  display: flex;
+  cursor: pointer;
+}
+
+li:hover {
+  background-color: #eee;
+}
+</style>
